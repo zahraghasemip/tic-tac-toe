8a9115d6ddbf56efca69f0f4fe0dc33bafc09516
Zahra Ghasemi <zahraghasemi2487@gmail.com>
2024-08-11 19:49:50 +0330

    move function added

--- game.py
+++ game.py
@@ -19,7 +19,27 @@ class TicTakToe :
                 self.buttons[row][col].grid(row=row, column=col)
 
     def make_move(self, row, col):
+        if self.board[row][col] == ' ':
+            self.board[row][col] = self.current_player
+            self.buttons[row][col].config(text=self.current_player)
+
+            if self.check_win(self.current_player):
+                pass
+            elif self.is_full():
+                pass
+            else:
+                self.current_player = 'O' if self.current_player == 'X' else 'X'
+
+        else:
+            messagebox.showerror("Invalid move", "Cell is already taken. Try again!")
+
+    def check_win(self, player):
         pass
+
+    def is_full(self):
+        pass
+    
+
 
     def run(self):
         self.window.mainloop()
